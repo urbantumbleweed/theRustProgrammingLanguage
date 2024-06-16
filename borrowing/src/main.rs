3f7acabf11604216126a671e0e6165022f8b15ab
Stephen Saunders <saunders.stephen@gmail.com>
2024-06-15 20:09:55 -0700

    use mutable borrow with shadowing to immutable

--- borrowing/src/main.rs
+++ borrowing/src/main.rs
@@ -1,10 +1,17 @@
 fn main() {
-    let reference_to_nothing: String = dangle();
-    
-    println!("{}", reference_to_nothing)
+    let mut s: String = String::from("hello");
+    change(&mut s);
+
+    let s = s;
+    let t = &s;
+    let t1 = &s;
+
+    println!("{}, {}", t, t1);
+
 }
 
-fn dangle() -> String {
-    let s = String::from("hello");
-    s
+fn change(s: &mut String) {
+    for _ in 0..10 {
+        s.push_str("-o");
+    }
 }
