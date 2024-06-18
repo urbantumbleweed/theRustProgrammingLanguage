108b1127b1649deb347f0a9a97cf9b0685e09f26
Stephen Saunders <saunders.stephen@gmail.com>
2024-06-17 21:44:34 -0700

    refactor: Update Rectangle struct to include Debug trait

--- exercises/rectangles/src/main.rs
+++ exercises/rectangles/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -17,5 +18,5 @@ fn main() {
         height: 50,
     };
 
-    println!("The area of the rectangle is {} square pixels", rect1.area());
+    println!("rect1 is {:#?}", rect1);
 }
