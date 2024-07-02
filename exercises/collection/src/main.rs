8f843ca801025907cb0e58c86fde371d87b34aed
Stephen Saunders <saunders.stephen@gmail.com>
2024-07-02 15:39:48 -0700

    refactor: Update main.rs to use indexing and matching for vector operations

--- exercises/collection/src/main.rs
+++ exercises/collection/src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
     let v: Vec<i32> = vec![1, 2, 3, 4, 5];
 
-    for i in v {
-        println!("{}", i);
-    }
+   let third: &i32 = &v[2];
+   print!("The third element is {}", third);
+
+   match v.get(10) {
+    Some(i) => println!("the option is {}", i),
+    None => println!("there is nothing here"),
+   }
 }
