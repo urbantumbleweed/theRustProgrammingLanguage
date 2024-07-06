4938fc19711801e7a7fb1c560717ceadd02e45f5
Stephen Saunders <saunders.stephen@gmail.com>
2024-07-06 15:01:59 -0700

    feature: add the ability to calculate the mode

--- exercises/collection/src/main.rs
+++ exercises/collection/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
 
     //given a list of integers
@@ -5,7 +7,7 @@ fn main() {
     // assume initially that the list is hardcoded
     // later, refactor to code to accept command line arguments
 
-    let input: [i32; 11] = [9, 23, -2, 87, 12, -1, 34, 25, 9, 15, 17];
+    let input: [i32; 12] = [9, 23, -2, 87, 12, -1, 34, 25, 9, 15, 17, 9];
 
     let mut list: Vec<i32> = input.to_vec();
     let sum: &i32 = &list.iter().fold(0, |sum, i| sum + i);
@@ -16,11 +18,34 @@ fn main() {
 
     list.sort_unstable();
     println!("Sorted list: {:#?} with length: {}", list, list.len());
-    let middle_index: usize = if list.len() % 2 == 0 {
-        (list.len() / 2) - 1
-    } else {
-        list.len() / 2
-    };
+    let middle_index = get_middle_index(&list);
     let median: &i32 = &list[*&middle_index];
     println!("The median of the set is: {}. Middle index: {}", &median, &middle_index);
+
+    // calculate the mode
+    let mut counts = HashMap::new();
+    let mut current_mode: i32 = 0;
+    let mut highest_count: i32 = 0;
+    for &value in list.iter() {
+        let count = counts.entry(value).or_insert(0);
+        *count += 1;
+        if *count > highest_count {
+            highest_count = *count;
+            current_mode = value;
+        }
+    }
+    println!("The mode of the set is: {}. Count: {}", &current_mode, &highest_count);
+}
+
+fn get_middle_index<T>(list: &Vec<T>) -> usize {
+    const HALF: i32 = 2;
+    const ZERO_BASED_INDEX_ADJUSTMENT: i32 = 1;
+    const EVEN_NUMBER: i32 = 0;
+    let length: i32 = list.len() as i32;
+
+    if &length % HALF == EVEN_NUMBER {
+        ((&length / HALF) - ZERO_BASED_INDEX_ADJUSTMENT) as usize
+    } else {
+        (&length / HALF) as usize
+    }
 }
