ac1619bff212d1d9deabf5c90ca778e97a3d7705
Stephen Saunders <saunders.stephen@gmail.com>
2024-07-06 13:53:32 -0700

    refactor: Update main.rs to calculate average using vector methods

--- exercises/collection/src/main.rs
+++ exercises/collection/src/main.rs
@@ -7,18 +7,8 @@ fn main() {
 
     let input: [i32; 10] = [9, 23, -2, 87, 12, -1, 34, 25, 9, 15];
 
-    let mut list: Vec<i32> = Vec::new();
-    fn 
-    for i in &input {
-        list.push(*i);
-    }
-    let iterator = list.iter();
-    let mut sum: i32 = 0;
-    for v in iterator {
-      sum += *v;
-    }
-    let average: f64 = sum as f64/ list.len() as f64;
+    let list: Vec<i32> = input.to_vec();
+    let sum: &i32 = &list.iter().fold(0, |sum, i| sum + i);
+    let average: f64 = *sum as f64/ list.len() as f64;
     println!("The average is: {}. Sum is: {}", &average, &sum);
-
-
 }
