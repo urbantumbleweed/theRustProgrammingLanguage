72ef4d08ec50a61e8431283c28f777bb09848ff6
Stephen Saunders <saunders.stephen@gmail.com>
2024-07-11 11:43:30 -0700

    refactor: Update pig_latin.rs to handle consonant words in pig latin

--- exercises/collection/src/bin/pig_latin.rs
+++ exercises/collection/src/bin/pig_latin.rs
@@ -1,39 +1,39 @@
 extern crate unicode_segmentation;
 
+use std::io;
 use unicode_segmentation::UnicodeSegmentation;
 
 fn main() {
   // given a hardcoded word, convert to pig latin (phase 1)
   // given a hardcoded sentence, convert to pig latin (phase 2)
   // given an enter terminated string on stdin, convert to pig latin (phase 3)
+  let mut s = String::new();
 
-  // phase 1
-  let word = String::from("first");
+  println!("Enter a string to Pig-Latinify:");
+  io::stdin().read_line(&mut s).expect("Error reading line");
 
-  // split into characters
-  let mut chars = UnicodeSegmentation::graphemes(word.as_str(),true)
-    .collect::<Vec::<&str>>();
-  // examine the first character (is it a consonant or vowel)
-  println!("pig latin word: {}", piggify(&mut chars));
-    // consonant
-      // remove the first character
-      // create suffix string (-fay)
-      // append sufix to remaining partial string
-      // return word
-    // vowel
-      // append vowel suffix (-hay) to word
-      // return word
+  let words: Vec<&str> = s.as_mut_str().split(' ').collect();
 
+  let mut output_list: Vec<String> = Vec::new();
+  for word in &words {
+    // split into characters
+    let mut chars = UnicodeSegmentation::graphemes(*word,true)
+      .collect::<Vec::<&str>>();
+    let _ = &output_list.push(piggify(&mut chars));
+  }
 
+  let output_str = output_list.join(" ");
+
+  println!("{output_str}");
 }
 
 fn piggify(word: &mut Vec<&str>) -> String {
-  let first_char: char = word.get(0).unwrap().chars().next().unwrap();
+  let first_char: char = word.get(0).expect("Could not unwrap Vec").chars().next().unwrap();
 
   match first_char {
-    'a'|'e'|'i'|'o'|'u'|'A'|'E'|'I'|'O'|'U' => handle_vowel(word),
-    _ => handle_consonant(word)
-  }
+    'a'|'e'|'i'|'o'|'u'|'A'|'E'|'I'|'O'|'U' => return handle_vowel(word),
+    _ => return handle_consonant(word)
+  };
 
   fn handle_vowel(word: &mut Vec<&str>) -> String {
     const HAY: &str = "-hay";
